403553aa88a607477f920656ecbd3bc1cc4a1e4c
Kukul <naththanicha@gmail.com>
2022-11-17 19:41:59 +0700

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import "../css/Navbar.css"
 import Navbar from "react-bootstrap/Navbar"
 import Container from 'react-bootstrap/Container';
 import { Nav } from "react-bootstrap";
+import imgmaintain1 from "../pic/maintain1.png"
 
-
-const NavbarHotel = () => {
+const NavbarHotel = (props) => {
     return (
         <Navbar className="navbar-class" expand="lg" >
             <Navbar.Brand href="/" className="logo-margin">logo</Navbar.Brand>
@@ -15,7 +15,9 @@ const NavbarHotel = () => {
                     <Nav.Link href="#" className="me-5">สมัครเป็นผู้ให้บริการ</Nav.Link>
                     <Nav.Link href="/about" className="me-5">ฟีดแบ็ก</Nav.Link>
                     <Nav.Link href="/about" className="me-5">เกี่ยวกับเรา</Nav.Link>
-                    <Nav.Link to="#"><button as="input" type="button" className="btn-register me-5">ลงทะเบียน</button></Nav.Link>
+                    {/* <Nav.Link to="#"><button as="input" type="button" className="btn-register me-5">ลงทะเบียน</button></Nav.Link> */}
+                    <Nav.Link to="#"><div className="circle-user me-5"><img src={imgmaintain1} alt="#" className="img-user-navbar"/></div></Nav.Link>
+
                 </Nav>
             </Navbar.Collapse>
 
